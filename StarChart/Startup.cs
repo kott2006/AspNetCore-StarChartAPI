2d9b12538fb5b6c7c48a0bce738156a6cf1b87c4
kott2006 <kott2006@outlook.com>
2020-05-26 22:40:18 -0400

    first checkin

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using StarChart.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace StarChart
 {
@@ -10,13 +14,18 @@ namespace StarChart
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddMvc();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseInMemoryDatabase("StartChart"));
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            
+            app.UseMvc();
         }
     }
 }
